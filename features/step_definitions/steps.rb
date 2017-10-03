8a4a228a635e147414a0b09c03e1c9f42d5317b2
Xavier Perez <xap5xap@users.noreply.github.com>
2017-10-03 12:16:58 -0500

    14

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -9,25 +9,55 @@ class Account
 end
 
 class Teller
+  def initialize(cash_slot)
+    @cash_slot=cash_slot
+  end
+
   def withdraw_from(account, amount)
+    @cash_slot.dispense(amount)
   end
 end
+
+class CashSlot
+  def contents
+    @contents or raise("I'm empty")
+  end
+
+  def dispense(amount)
+    @contents = amount
+  end
+end
+
+module KnowsTheDomain
+  def my_account
+    @my_account||= Account.new
+  end
+  
+  def cash_slot
+    @cash_slot ||= CashSlot.new
+  end
+
+  def teller
+  @teller ||= Teller.new(cash_slot) 
+  end
+end
+
+World(KnowsTheDomain)
 
 CAPTURE_CASH_AMOUNT = Transform(/^\$(\d+)$/) do |number|
   number.to_i
 end
 
 Given(/^I have deposited (#{CAPTURE_CASH_AMOUNT}) in my account$/) do |amount|
-  my_account = Account.new
+  # puts self
   my_account.deposit(amount)
   expect(my_account.balance).to eq(amount), "Expected the balance to be #{amount} but ot was #{my_account.balance}"
 end
 
 When(/^I withdraw (#{CAPTURE_CASH_AMOUNT})$/) do |amount|
-  teller = Teller.new
   teller.withdraw_from(my_account, amount)
 end
 
-Then(/^\$(\d+) should be dispensed$/) do |amount|
-  pending ("How do we validate that cash was dispensed?")
+Then(/^(#{CAPTURE_CASH_AMOUNT}) should be dispensed$/) do |amount|
+  expect(cash_slot.contents).to eq(amount)
 end
