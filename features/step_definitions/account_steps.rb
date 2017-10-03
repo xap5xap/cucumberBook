685ca3221a3318b079932c0576ba4f523292840c
Xavier Perez <xap5xap@users.noreply.github.com>
2017-10-03 14:01:11 -0500

    2

--- features/step_definitions/account_steps.rb
+++ features/step_definitions/account_steps.rb
@@ -1,5 +1,7 @@
-Given(/^I have deposited (#{CAPTURE_CASH_AMOUNT}) in my account$/) do |amount|
-  # puts self
-  my_account.deposit(amount)
-  expect(my_account.balance).to eq(amount), "Expected the balance to be #{amount} but ot was #{my_account.balance}"
+Given(/^my account has been credited with (#{CAPTURE_CASH_AMOUNT})$/) do |amount|
+  my_account.credit(amount)
+end
+
+Then(/^the balance of my account should be (#{CAPTURE_CASH_AMOUNT})$/) do |amount|
+    expect(my_account.balance).to eq(amount), "Expected the balance to be #{amount} but ot was #{my_account.balance}"
 end
--- lib/nice_bank.rb
+++ lib/nice_bank.rb
@@ -1,10 +1,14 @@
 class Account
- def deposit(amount)
+ def credit(amount)
   @balance=amount
  end
 
  def balance
   @balance
+ end
+
+ def debit(amount)
+  @balance -= amount
  end
 end
 
@@ -14,6 +18,7 @@ class Teller
   end
 
   def withdraw_from(account, amount)
+    account.debit(amount)
     @cash_slot.dispense(amount)
   end
 end
